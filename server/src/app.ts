9113f63f8a2920d8359f0a5310e53bee1fe4eb06
Jean-Aime-2025 <tVzSjwlYVnKZRYd>
2025-05-20 00:56:50 +0200

    update : logs - winston

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,8 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger/doc/swagger.json';
 import ServerResponse from './utils/ServerResponse';
 import router from './routes';
+import { requestLogger } from './middlewares/log.middleware';
+import logger from './utils/logger';
 
 config();
 
@@ -19,12 +21,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({ origin: "*" }))
 app.disable('x-powered-by');
 
+app.use(requestLogger);
+
 app.use('/api/v1', router)
 // @ts-ignore: Unreachable code error
 app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use("*", (req, res) => {
     return ServerResponse.error(res, "Route not found")
 })
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  logger.error(`${err.message} - ${req.method} ${req.originalUrl}`);
+  res.status(500).json({ message: "Internal server error" });
+});
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
